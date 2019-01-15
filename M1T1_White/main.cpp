064d47e1e007944c65f8b0446fb69ac61b5d4e71
whitem1396 <46724878+whitem1396@users.noreply.github.com>
2019-01-15 14:19:00 -0500

    First upload

---
+++ M1T1_White/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// CSC 134
+// M1T1
+// White
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello From CSC134!" << endl;
+    cout << "Hi my name is...." << endl;
+    cout << "Sorry I seem t- -e b---ki-g u-" << endl;
+    return 0;
+}
