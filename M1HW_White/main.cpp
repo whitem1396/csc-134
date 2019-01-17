8ef78f71711e8168e1f239b1b6fd00f1962d6271
whitem1396 <46724878+whitem1396@users.noreply.github.com>
2019-01-17 13:59:28 -0500

    CSC-134 M1HW1

---
+++ M1HW_White/main.cpp
@@ -0,0 +1,23 @@
+// CSC 134
+// M1HW1
+// Matthew White
+// 1/17/19
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Provide a brief introduction
+
+    cout << "Hello my name is Matthew White" << endl;
+    cout << "I plan to graduate in " << 2021 << endl;
+    cout << "I have " << 4 << " pets:" << endl;
+    cout << "\t2 dogs" << endl;
+    cout << "\t1 bunny" << endl;
+    cout << "\t1 fish" << endl;
+    cout << endl;
+    cout << "testing\t1\t2\t3\t123\t321" << endl;
+    return 0;
+}
