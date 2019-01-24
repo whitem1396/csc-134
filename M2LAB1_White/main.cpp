d3c45850c2a45811b26727ad52b4e8b32ce6823c
whitem1396 <46724878+whitem1396@users.noreply.github.com>
2019-01-24 13:53:32 -0500

    M2LAB1_White #2

---
+++ M2LAB1_White/main.cpp
@@ -0,0 +1,40 @@
+// CSC 134
+
+// M2LAB
+
+// Matthew White
+
+// 1/22/19
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string movieName;
+    double rating;
+    int year;
+    char firstLetter;
+
+    movieName = "Glass";
+    rating = 7.1;
+    year = 2019;
+    firstLetter = 'G';
+
+ cout << "The movies name is: ";
+ getline(cin, movieName);
+ cout << "It was released in the year: ";
+ cin >> year;
+ cout << "It has a rating of: ";
+ cin >> rating;
+ cout << "I would file this movie under: ";
+ cin >> firstLetter;
+
+ cout << "The movies name is " << movieName << endl;
+ cout << "It was released in the year " << year << "." << " It has a rating of " << rating <<endl;
+ cout << "I would file this movie under " << firstLetter << endl;
+
+
+    return 0;
+}
