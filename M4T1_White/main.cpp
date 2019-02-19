adb124414131026a349f58d86109342030a7d89b
whitem1396 <46724878+whitem1396@users.noreply.github.com>
2019-02-19 14:02:58 -0500

    M4T1_White CSC 134

---
+++ M4T1_White/main.cpp
@@ -0,0 +1,18 @@
+// CSC 134
+// M4T1 Listings
+// 02/19/2019
+// Matthew White
+
+#include <iostream>
+
+using namespace std;
+
+void BigDog(int KibblesCount);
+
+int main()
+{
+    BigDog(9);
+    return 0;
+}
+
+
