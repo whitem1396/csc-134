cd7624e88306971f90682ffb2349b68084c863a2
whitem1396 <46724878+whitem1396@users.noreply.github.com>
2019-02-19 14:03:54 -0500

    M4T1#2_White CSC 134

---
+++ M4T1#2_White/main.cpp
@@ -0,0 +1,17 @@
+// CSC 134
+// M4T1 #2 Listings
+// 02/19/2019
+// Matthew White
+
+#include <iostream>
+#include "safestuff.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Surprise, surprise!" << endl;
+    cout << "The combination is (once again)" << endl;
+    cout << SafeCracker(12) << endl;
+    return 0;
+}
